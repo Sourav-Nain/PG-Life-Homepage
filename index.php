4cecfbb4130fe040e82b7373c4aa8ac4be8e59fb
Sourav-Nain <souravnain0003@gmail.com>
2026-07-19 21:41:56 +0530

    new changes

--- index.php
+++ index.php
@@ -5,85 +5,118 @@ if (isset($_GET['city'])) {
     $city = $_GET['city'];
 }
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>PG Life</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
-</head>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+    />
+    <link rel="stylesheet" href="style.css" />
+  </head>
 
-<body>
+  <body>
     <header>
-        <div class="navbar">
-            <div class="logo">
-                <div class="logo1"></div>
-            </div>
-            <div class="login">
-                <i class="fa-solid fa-user"></i>
-                <a href="signup.php" class="signup">Signup</a>
-            </div>
-            <div class="login">
-                <i class="fa-solid fa-arrow-right-to-bracket"></i>
-                <a href="login.php">Login</a>
-            </div>
+      <div class="navbar">
+        <div class="logo">
+          <div class="logo1"></div>
         </div>
+        <div class="login">
+          <i class="fa-solid fa-user"></i>
+          <a href="signup.php">Signup</a>
+        </div>
+        <div class="login">
+          <i class="fa-solid fa-arrow-right-to-bracket"></i>
+          <a href="login.php">Login</a>
+        </div>
+      </div>
     </header>
     <div class="hero-section">
-        <h2>Happiness per Square Foot</h2>
-        <div class="input-box">
-            <input type="text" placeholder="Enter your City to search for PGs">
-            <div class="search-icon">
-                <i class="fa-solid fa-magnifying-glass"></i>
-            </div>
+      <h2>Happiness per Square Foot</h2>
+      <div class="input-box">
+        <input type="text" placeholder="Enter your City to search for PGs" />
+        <div class="search-icon">
+          <i class="fa-solid fa-magnifying-glass"></i>
         </div>
+      </div>
     </div>
     <div class="heading-major">
-        <h1>Major Cities</h1>
+      <h1>Major Cities</h1>
     </div>
     <div class="major-cities">
-        <a href="PG-Life-Property-Listing/index.php?city=Mumbai">
-            <div class="cities">
-                <div class="city-logo" style="background-image: url('mumbai.png');"></div>
-            </div>
-        </a>
-        <a href="PG-Life-Property-Listing/index.php?city=Delhi">
-            <div class="cities">
-                <div class="city-logo" style="background-image: url('delhi.png');"></div>
-            </div>
-        </a>
-        <a href="PG-Life-Property-Listing/index.php?city=Bengaluru">
-            <div class="cities">
-                <div class="city-logo" style="background-image: url('bangalore.png');"></div>
-            </div>
-        </a>
-        <a href="PG-Life-Property-Listing/index.php?city=Chennai">
-            <div class="cities">
-                <div class="city-logo" style="background-image: url('chennai.png');"></div>
-            </div>
-        </a>
+      <a href="PG-Life-Property-Listing/index.php?city=Mumbai">
+        <div class="cities">
+          <div
+            class="city-logo"
+            style="background-image: url(&quot;mumbai.png&quot;)"
+          ></div>
+        </div>
+      </a>
+      <a href="PG-Life-Property-Listing/index.php?city=Delhi">
+        <div class="cities">
+          <div
+            class="city-logo"
+            style="background-image: url(&quot;delhi.png&quot;)"
+          ></div>
+        </div>
+      </a>
+      <a href="PG-Life-Property-Listing/index.php?city=Bengaluru">
+        <div class="cities">
+          <div
+            class="city-logo"
+            style="background-image: url(&quot;bangalore.png&quot;)"
+          ></div>
+        </div>
+      </a>
+      <a href="PG-Life-Property-Listing/index.php?city=Chennai">
+        <div class="cities">
+          <div
+            class="city-logo"
+            style="background-image: url(&quot;chennai.png&quot;)"
+          ></div>
+        </div>
+      </a>
     </div>
     <footer>
-        <div class="footer-city">
-            <div class="city"><a href="PG-Life-Property-Listing/index.php?city=Mumbai">PG in Mumbai</a></div>
-            <div class="city"><a href="PG-Life-Property-Listing/index.php?city=Delhi">PG in Delhi</a></div>
-            <div class="city"> <a href="PG-Life-Property-Listing/index.php?city=Bengaluru">PG in Bengaluru</a></div>
-            <div class="city"><a href="PG-Life-Property-Listing/index.php?city=Chennai">PG in Chennai</a></div>
+      <div class="footer-city">
+        <div class="city">
+          <a href="PG-Life-Property-Listing/index.php?city=Mumbai"
+            >PG in Mumbai</a
+          >
         </div>
-        <div class="complaint">
-            <p>Launch a Complaint</p>
+        <div class="city">
+          <a href="PG-Life-Property-Listing/index.php?city=Delhi"
+            >PG in Delhi</a
+          >
         </div>
-        <div class="copyright">
-            <i class="fa-regular fa-copyright"></i>
-            <p>2026 copyright PG Life</p>
+        <div class="city">
+          <a href="PG-Life-Property-Listing/index.php?city=Bengaluru"
+            >PG in Bengaluru</a
+          >
         </div>
-        <div class="footer-logo">
-            <div class="footer-logo1"></div>
+        <div class="city">
+          <a href="PG-Life-Property-Listing/index.php?city=Chennai"
+            >PG in Chennai</a
+          >
         </div>
+      </div>
+      <div class="complaint">
+        <p>Launch a Complaint</p>
+      </div>
+      <div class="copyright">
+        <i class="fa-regular fa-copyright"></i>
+        <p>2026 copyright PG Life</p>
+      </div>
+      <div class="footer-logo">
+        <div class="footer-logo1"></div>
+      </div>
     </footer>
-</body>
 
+    <div class="overlay" id="overlay">
+      <div class="form-container" id="form-container"></div>
+    </div>
+  </body>
 </html>
--- login.php
+++ login.php
@@ -38,53 +38,74 @@ if(isset($_POST['email']) && isset($_POST['password'])){
 }
 
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+    />
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Login Page</title>
-</head>
+  </head>
 
-<body>
-    <?php
-    if(!empty($message)){
-        ?>
-        <div class="w-[40%] mx-auto mt-4 p-3 bg-green-100 text-green-700 border border-green-500 rounded text-center">
-            <?php
-            echo $message ?>
-            </div> <?php
-    }
-    ?>
+  <body>
+    <?php if(!empty($message)){ ?>
+    <div
+      class="w-[40%] mx-auto mt-4 p-3 bg-green-100 text-green-700 border border-green-500 rounded text-center"
+    >
+      <?php echo $message ?>
+    </div>
+    <?php } ?>
     <form action="login.php" method="POST">
-        <div class="flex w-[40%] m-auto p-4 text-xl">
-            <h1 class="w-[95%] font-serif text-2xl">Login with PGLife</h1>
-            <a href="index.php">
-                <p><i class="fa-solid fa-x w-[5%] text-gray-500 cursor-pointer"></i></p>
-            </a>
+      <div class="flex w-[40%] m-auto p-4 text-xl">
+        <h1 class="w-[95%] font-serif text-2xl">Login with PGLife</h1>
+        <a href="index.php">
+          <p>
+            <i class="fa-solid fa-x w-[5%] text-gray-500 cursor-pointer"></i>
+          </p>
+        </a>
+      </div>
+      <div class="flex flex-col m-auto w-[40%] gap-2 h-44">
+        <div class="flex border-2 border-gray-400 rounded-md h-[33%]">
+          <i
+            class="fa-solid fa-envelope w-[8%] bg-gray-400 flex justify-center items-center text-gray-700 border-gray-400 rounded-tl rounded-bl"
+          ></i>
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            class="w-[91%] pl-2"
+          />
         </div>
-        <div class="flex flex-col m-auto w-[40%] gap-2 h-44">
-            <div class="flex border-2 border-gray-400 rounded-md h-[33%]">
-                <i
-                    class="fa-solid fa-envelope w-[8%] bg-gray-400 flex justify-center items-center text-gray-700 border-gray-400 rounded-tl rounded-bl"></i>
-                <input type="email" placeholder="Email" name="email" class="w-[91%] pl-2">
-            </div>
-            <div class="flex border-2 border-gray-400 rounded-md h-[33%]">
-                <i
-                    class="fa-solid fa-lock w-[8%] bg-gray-400 flex justify-center items-center text-gray-700 border-r-2 border-gray-400 rounded-tl rounded-bl"></i>
-                <input type="password" placeholder="Password" name="password" class="w-[91%] pl-2">
-            </div>
-            <div class="login-btn h-[33%]">
-            <button type="submit" class="text-center w-full p-1 bg-violet-400 rounded-md">Login</button>
+        <div class="flex border-2 border-gray-400 rounded-md h-[33%]">
+          <i
+            class="fa-solid fa-lock w-[8%] bg-gray-400 flex justify-center items-center text-gray-700 border-r-2 border-gray-400 rounded-tl rounded-bl"
+          ></i>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            class="w-[91%] pl-2"
+          />
         </div>
+        <div class="login-btn h-[33%]">
+          <button
+            type="submit"
+            class="text-center w-full p-1 bg-violet-400 rounded-md"
+          >
+            Login
+          </button>
         </div>
-        <div class="w-[40%]  mx-auto my-4 flex justify-center">
-            <p><a class="text-blue-700" href="signup.php">Click here </a> to register a new Account</p>
-        </div>
+      </div>
+      <div class="w-[40%] mx-auto my-4 flex justify-center">
+        <p>
+          <a class="text-blue-700" href="signup.php">Click here </a> to register
+          a new Account
+        </p>
+      </div>
     </form>
-</body>
-
+  </body>
 </html>
